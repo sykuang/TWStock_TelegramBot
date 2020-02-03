29893ac8a2f237fc85694ea82f5c651bfc1e96e6
sykuang <sykuang.tw@gmail.com>
2020-02-04 00:10:50 +0800

    Optimize Dataprovide

--- DataProvider/fugle.py
+++ DataProvider/fugle.py
@@ -7,24 +7,28 @@ class realTimeProvider:
 
     def getStockInfo(self, id):
         ret = dict()
-        output = intraday.trades(apiToken=self.apiToken,
-                                 output="dataframe", symbolId=id)
         output_meta = intraday.meta(
             apiToken=self.apiToken, output="dataframe", symbolId=id)
         output_chart = intraday.chart(
             apiToken=self.apiToken, output="dataframe", symbolId=id)
-        graph=output_chart.plot(x="at",y='open')
+        ref_price=output_meta.iloc[0,
+                                       output_meta.columns.get_loc("priceReference")]
+        print(ref_price)
+        current_price=output_chart.iloc[-1, output_chart.columns.get_loc("close")]
+        if ref_price>current_price:
+            graph=output_chart.plot(x="at",y='close',color='green')
+        elif ref_price==current_price:
+            graph=output_chart.plot(x="at",y='close',color='gray')
+        else:
+            graph=output_chart.plot(x="at",y='close',color='red')
+        graph.axhline(ref_price, linestyle='dashed', color='xkcd:dark grey', alpha=0.6, label='參考價', marker='')
+        graph.legend().set_visible(False)
         fig = graph.get_figure()
         buf=io.BytesIO()
         fig.savefig(buf,format='png')
-        # plt.show()
-        # except:
-        # print("Get Data Error")
-        # raise Exception("Get Data Error")
         ret['Name'] = output_meta.iloc[0,
                                        output_meta.columns.get_loc("nameZhTw")]
-        ret['RealPrice'] = str(
-            output.iloc[-1, output.columns.get_loc("price")])
+        ret['RealPrice'] = str(current_price)
         ret['ID'] = id
         ret['photo']=buf
         return ret
